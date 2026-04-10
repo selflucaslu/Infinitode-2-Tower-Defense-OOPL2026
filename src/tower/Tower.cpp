a120402ed3ce4d5b7d9e465fc87feb4d686b4771
ZoeChengYu <0931426632yang50@gmail.com>
2026-04-10 14:24:20 +0800

    make demo Tower.cpp

---
+++ src/tower/Tower.cpp
@@ -0,0 +1,56 @@
+#include <./tower/Tower.hpp>
+
+Tower::Tower(std::string spriteId, int originX, int originY, float attack_range, int Level, TowerType type)
+{
+
+}
+
+Tower::~Tower()
+{
+
+}
+
+int Tower::GetX() const
+{
+
+}
+
+int Tower::GetY() const
+{
+
+}
+
+std::string Tower::GetSpriteID() const
+{
+
+}
+
+float Tower::GetAttackRange() const
+{
+
+}
+
+int Tower::GetLevel() const
+{
+
+}
+
+Tower::TowerType Tower::GetTypes() const
+{
+
+}
+
+void Tower::SetAttackRange(float new_attack_range)
+{
+
+}
+
+void Tower::SetLevel(int new_Level)
+{
+
+}
+
+void Tower::setPosition(float newX, float newY)
+{
+
+}
