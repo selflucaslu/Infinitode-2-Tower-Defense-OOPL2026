4beb3531b22779fcc0e831bbcaa02d29a9c74fbe
ZoeChengYu <0931426632yang50@gmail.com>
2026-04-14 23:29:20 +0800

    fix my  code logic bug :tower.hpp

--- include/tower/AmmoTower.hpp
+++ include/tower/AmmoTower.hpp
@@ -5,6 +5,6 @@ class AmmoTower:public Tower
 {
     std::string AmmoID;
     float AmmoSpeed=10.0f,AmmoPower=10.0f;
-
+    
     
 };
--- include/tower/Tower.hpp
+++ include/tower/Tower.hpp
@@ -6,16 +6,16 @@
 #include <string>
 
 class Tower{
-    enum TowerType {
-        AMMO,//只要會射都叫AMMO
-        AROUND_SKILL//用範圍技的都叫AROUND_SKILL
-    }type;
     std::string TowerId = "tower-basic";
     int Level=1;
     int gridX=0; // 格子 x
     int gridY=0; // 格子 y
     float attack_range=10.0f;
     public:
+        enum TowerType {
+        AMMO,//只要會射都叫AMMO
+        AROUND_SKILL//用範圍技的都叫AROUND_SKILL
+        }type;
         Tower(std::string TowerId,int gridX=0,int gridY=0,float attack_range=10.0f,int Level=1,TowerType type=AMMO);
         ~Tower();
         std::string GetTowerID() const;
