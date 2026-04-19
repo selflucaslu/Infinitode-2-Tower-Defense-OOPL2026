1070ab6716b795cfee7d809a7bf89b9204ecca76
ZoeChengYu <0931426632yang50@gmail.com>
2026-04-20 00:45:07 +0800

    try fix:my driver can run code (MSI Thin 15 B13UC-1418TW)

--- include/utils/FpsOverlay.hpp
+++ include/utils/FpsOverlay.hpp
@@ -20,7 +20,8 @@ public:
     void display();
 
 private:
-    static constexpr const char* kFontPath = "PTSD/assets/fonts/Inter.ttf"; // 字體路徑
+    // Change this:
+    static constexpr const char* kFontPath = "assets/fonts/Inter.ttf";
     static constexpr int kFontSize = 24; // 字體大小
     static constexpr float kZIndex = 3.0F; // 疊圖的 Z 索引，確保在其他 UI 元素之上
     static constexpr float kRefreshInterval = 0.2F; // 更新 FPS 顯示的時間間隔（秒）
